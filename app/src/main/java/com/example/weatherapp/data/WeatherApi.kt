3b678bc88a9b0b42452e287a7325e17bd210e849
Harry Kachigamba <harrykachigamba22@outlook.com>
2025-01-23 06:37:03 -0800

    Added data API key, base url, interface and object

--- app/src/main/java/com/example/weatherapp/data/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/data/WeatherApi.kt
@@ -1,2 +1,28 @@
 package com.example.weatherapp.data
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY = "690355666af781e18399d81ec0c913b2"
+const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+interface WeatherApi {
+    @GET("weather")
+    suspend fun getWeather(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String = API_KEY,
+        @Query("units") units: String = "metric"
+    ): WeatherData
+}
+
+object RetrofitInstance {
+    val api: WeatherApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
+}
