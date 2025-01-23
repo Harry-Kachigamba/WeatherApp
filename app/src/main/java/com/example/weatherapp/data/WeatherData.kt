b51dca49118e426148f6fdb3387e8f7f64940284
Harry Kachigamba <harrykachigamba22@outlook.com>
2025-01-23 06:07:26 -0800

    Added data classes

--- app/src/main/java/com/example/weatherapp/data/WeatherData.kt
+++ app/src/main/java/com/example/weatherapp/data/WeatherData.kt
@@ -1,2 +1,16 @@
 package com.example.weatherapp.data
 
+data class WeatherData(
+    val name: String,
+    val main: Main,
+    val weather: List<Weather>
+)
+
+data class Main(
+    val temp: Double
+)
+
+data class Weather(
+    val description: String,
+    val icon: String
+)
